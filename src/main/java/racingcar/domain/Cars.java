2105982c14e435585a288784edae15d54e9280f1
김민정 <mjeong@sookmyung.ac.kr>
2023-12-13 10:12:15 +0900

    feat: 우승자 찾는 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,17 +2,34 @@ package racingcar.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.util.RandomNumberGenerator;
 
 public class Cars {
     private static final String DELIMITER = "\n";
     private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
-        this.cars = cars;
+    public Cars(List<Name> carNames) {
+        this.cars = carNames.stream()
+                .map(name -> new Car(name, new RandomNumberGenerator()))
+                .toList();
     }
 
-    public void playRound() {
+    public void moveAll() {
         cars.forEach(Car::moveOnCondition);
+    }
+
+    public List<String> getCarNamesWithMaxDistance() {
+        Car maxCar = getCarWithMaxDistance();
+        return cars.stream()
+                .filter(car -> car.isSameDistance(maxCar))
+                .map(Car::toNameString)
+                .toList();
+    }
+
+    private Car getCarWithMaxDistance() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     @Override
